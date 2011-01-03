3008ffb7371d8a94646a52b626cbfca903abda10
Robert Kosara <rkosara@me.com>
2011-01-02 21:46:24 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,27 @@
+# About
+
+The Nike+ GPS app is great when starting to run, but it's not something for the long term. One problem with switching to other programs or websites is that you will probably want to take your data with you. While there is no official way to do this, it is possible to coax the website into surrendering your data in a JSON format. This script converts that data into the [GPS Exchange Format (GPX)](http://www.topografix.com/gpx.asp), which most running websites and programs accept.
+
+# Procedure for Converting the Data
+
+You will have to do this manually for every run. Go to the runs tab on the Nike+ website and click on a run you want to export. In the URL bar, you will see something like this:
+
+http://nikerunning.nike.com/nikeos/p/nikeplus/en_US/plus/#//runs/detail/__&lt;user-id&gt;__/__&lt;run-id&gt;__/all/allRuns/
+
+Where the user-id and run-id are numerical. Use wget or curl to retrieve the following URL to a file (replacing the run-id part with your run-id):
+
+http://nikerunning.nike.com/nikeplus/v2/services/app/get\_gps\_detail.jsp?\_plus=true&id=__&lt;run-id&gt;__&format=json
+
+Name the file so that it ends in .json, because the script will produce a file with the same name and a .gpx extension (and it doesn't check whether the extension is correct or not). Run the script:
+
+    ./nike2gpx filename.json
+
+The resulting .gpx file can be imported to RunKeeper, etc.
+
+# Caveats
+
+This is a very simple (and ugly) script that produces a fairly basic GPX file. I make no claims to the general correctness of the output, but [RunKeeper](http://runkeeper.com/) accepts the files.
+
+# License
+
+Nike2GPX was written by Robert Kosara. I am placing it into the public domain.
---
+++ nike2gpx.py
@@ -0,0 +1,95 @@
+#!/usr/bin/python
+
+#
+# Nike2GPX by Robert Kosara. This program is in the public domain.
+#
+# This is a very simple script to convert JSON data from the Nike Running Site into GPX,
+# which can be imported into other programs and sites (like RunKeeper)
+# 
+# Download the JSON using this URL schema (replace the run-id with your run ID)
+# http:#nikerunning.nike.com/nikeplus/v2/services/app/get_gps_detail.jsp?_plus=true&id=<run-id>&format=json
+# 
+# Save it to a file that ends in .json, and run this script: ./nike2gpx <filename>.json
+# This will create a file <filename>.gpx in GPX format
+#
+
+from xml.etree.ElementTree import TreeBuilder, ElementTree
+from datetime import datetime
+import json
+import sys
+
+data = json.load(open(sys.argv[1], "r"))
+
+route = data["plusService"]["route"]
+
+builder = TreeBuilder()
+
+gpxAttrs = { "version":"1.1", "creator":"Nike2GPX", "xmlns:xsi":"http:#www.w3.org/2001/XMLSchema-instance", "xmlns":"http:#www.topografix.com/GPX/1/1", "xsi:schemaLocation":"http:#www.topografix.com/GPX/1/1 http:#www.topografix.com/gpx/1/1/gpx.xsd" }
+builder.start("gpx", gpxAttrs)
+
+builder.start("metadata", {})
+
+builder.start("name", {})
+builder.data("Run " + sys.argv[1][:-5])
+builder.end("name")
+
+minLon = 10000
+maxLon = -10000
+minLat = 10000
+maxLat = -10000
+for waypoint in route["waypointList"]:
+	if waypoint["lon"] > maxLon:
+		maxLon = waypoint["lon"]
+	if waypoint["lon"] < minLon:
+		minLon = waypoint["lon"]
+
+	if waypoint["lat"] > maxLat:
+		maxLat = waypoint["lat"]
+	if waypoint["lat"] < minLat:
+		minLat = waypoint["lat"]
+
+bounds = {"minlat":str(minLat), "maxlat":str(maxLat), "minlon":str(minLon), "maxlon":str(maxLon)}
+builder.start("bounds", bounds)
+builder.end("bounds")
+
+builder.end("metadata")
+
+builder.start("trk", {})
+
+builder.start("name", {})
+builder.data(sys.argv[1][:-5])
+builder.end("name")
+
+builder.start("type", {})
+builder.data("Run")
+builder.end("type")
+
+builder.start("trkseg", {})
+
+for waypoint in route["waypointList"]:
+
+	coords = {"lat":str(waypoint["lat"]), "lon":str(waypoint["lon"])}
+	builder.start("trkpt", coords)
+	
+	builder.start("ele", {})
+	builder.data(str(waypoint["alt"]))
+	builder.end("ele")
+	
+	builder.start("time", {})
+	time = datetime.utcfromtimestamp(waypoint["time"]/1000)
+	builder.data(time.strftime("%Y-%m-%dT%H:%M:%SZ"))
+	builder.end("time")
+	
+	builder.end("trkpt")
+
+builder.end("trkseg")
+
+builder.end("trk")
+
+builder.end("gpx")
+
+root = builder.close()
+
+tree = ElementTree(root)
+
+tree.write(open(sys.argv[1][:-5]+".gpx", "wb"), "utf-8")
